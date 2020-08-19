287ecddcc6b8d4252e8291f04a3b2e37e2d97c0a
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-08-19 20:43:25 +0800

    Added a slider view controller for InfoNews and InfoGraph - Code used from https://github.com/erthru/SlidingTabsExample

---
+++ Symptracker/InfoNewsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SimpleItemViewControllerOne.swift
+//  SlidingTabExample
+//
+//  Created by Suprianto Djamalu on 03/08/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoNewsViewController: UIViewController{
+    
+    private let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI(){
+        // view
+        view.backgroundColor = .white
+        view.addSubview(label)
+        
+        // label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.text = "InfoNewsViewController"
+    }
+    
+}

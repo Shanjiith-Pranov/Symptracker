f0e3c550c7cdc864d0776f0807f0e3af44f72b4d
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-08-22 18:24:22 +0800

    Fixed the issue

---
+++ Symptracker/Storyboards/QuickAcessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuickAcessViewController.swift
+//  Symptracker
+//
+//  Created by Shanjiith Pranov on 22/8/20.
+//  Copyright © 2020 sst. All rights reserved.
+//
+
+import UIKit
+
+class QuickAcessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

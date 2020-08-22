e2a9156627f042b287b73d67f78f1631ad77dfa3
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-08-22 17:56:41 +0800

    Fixed the issues i made without realising

--- Symptracker/View controllers/Info VIew controllers/InfoViewController.swift
+++ Symptracker/View controllers/Info VIew controllers/InfoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InfoViewController: UISimpleSlidingTabController {
+class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

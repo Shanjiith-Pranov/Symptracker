ca8e5732ea7111d9cc8bf1c41e6905a8418ebad2
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-08-31 21:12:40 +0800

    In the middle of another change

--- Symptracker/Storyboards/QuickAcessViewController.swift
+++ Symptracker/Storyboards/QuickAcessViewController.swift
@@ -9,22 +9,52 @@
 import UIKit
 
 class QuickAcessViewController: UIViewController {
+    
+    
+    var viruses = ["Covid","Hello","oh no its corona time"]
 
     @IBOutlet var tap: UITapGestureRecognizer!
     @IBOutlet weak var news: UITableView!
     @IBOutlet weak var cases: UIView!
     @IBOutlet weak var virusSelector: UIView!
+    @IBOutlet weak var virusTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         virusSelector.isHidden = true
-        
     }
     
     @IBAction func tapped(_ sender: Any) {
         virusSelector.isHidden = false
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viruses.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "virusCells", for: indexPath)
+
+        let currentVirus = viruses[indexPath.row]
+        if let cell = cell as? Virus{
+            cell.nameLabel.text = currentFriend.name
+            cell.ageLabel.text = "\(currentFriend.age)"
+            cell.schoolLabel.text = currentFriend.school
+            cell.profileImageView.image = UIImage(named: currentFriend.imageName)
+        }
+
+        return cell
+    }
+    
     
 
     
---
+++ Symptracker/Storyboards/VirusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VirusTableViewCell.swift
+//  Symptracker
+//
+//  Created by Shanjiith Pranov on 31/8/20.
+//  Copyright © 2020 sst. All rights reserved.
+//
+
+import UIKit
+
+class VirusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

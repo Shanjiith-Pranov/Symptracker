4cd5aafe34a06e1255443b322183633b98c7757e
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-09-09 00:27:09 +0800

    Added in the credits text

--- Symptracker/Storyboards/CreditsViewController.swift
+++ Symptracker/Storyboards/CreditsViewController.swift
@@ -15,7 +15,7 @@ class CreditsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        credits.text = "I would like to thank everyone for working in this project( the group. \n idk what to write for the credits"
+        credits.text = "This is our Team:\n\nHong Tuck Yan Christopher: Marketer (Under ADMT)\n\nJerick Seng: Business Developer (Under INE)\n\nShanjiith Pranov: Coder (Under ICT)\n\nSung Yeji: Marketer (Under ADMT)\n\n\n\nWe will like to take this moment to thank our Teachers fro the Changemaker department\n\nArtsDesignMediaTechnology: Mr Soh Wee Peng\n\nInnovation&Entrepreneurship : Mr Lam Him Chew\n\nInfoCommTechnology: Mr Robin Pang\n\n They have put in their heart and soul into assisting us with the right techniques in order to accomplish our tasks,  and they also helped with with any problems we had, being very patient. The feedback they provided were very honest and effective in our succession.\n"
     }
     
 

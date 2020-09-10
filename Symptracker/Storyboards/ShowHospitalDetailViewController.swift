b62d358b5d4835a7080afb2125fdd21c8e446b20
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-09-11 01:31:40 +0800

    Removed the need of another view controller as I was unable to send data over a segue

---
+++ Symptracker/Storyboards/ShowHospitalDetailViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ShowHospitalDetailViewController.swift
+//  Symptracker
+//
+//  Created by Shanjiith Pranov on 11/9/20.
+//  Copyright © 2020 sst. All rights reserved.
+//
+
+import UIKit
+
+class ShowHospitalDetailViewController: UIViewController {
+    
+    let hospitalData = [
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+    ]
+    
+    var number = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print(number)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

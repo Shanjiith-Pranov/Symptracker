17ca39187b76b811f3bb59de1a879db476b9bbd2
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2020-09-10 14:32:42 +0800

    In the middle of something

--- Symptracker/View controllers/HospitalsViewController.swift
+++ Symptracker/View controllers/HospitalsViewController.swift
@@ -7,24 +7,181 @@
 //
 
 import UIKit
+import MapKit
 
-class HospitalsViewController: UIViewController {
+class HospitalsViewController: UIViewController, MKMapViewDelegate {
+    
+    let hospitalData = [
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+        HospitalData(link: "hello", contacts: "", image: ""),
+    ]
+    
+    let hospitalAnnotations = HospitalAnnotations()
+    var selectedAnnotation: MKPointAnnotation?
+         let locationManager = CLLocationManager()
+    let initialCoordinate = CLLocationCoordinate2DMake(1.290270,103.851959)
+        var regionInMeters:Double = 10000
+    @IBOutlet weak var mapView: MKMapView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView = MKMarkerAnnotationView()
+        guard let annotation = annotation as? HospitalAnnotation else {return nil}
+        var identifier = ""
+        var color = UIColor.red
 
-        // Do any additional setup after loading the view.
+        if let dequedView = mapView.dequeueReusableAnnotationView(
+            withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            annotationView = dequedView
+        } else{
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        annotationView.markerTintColor = color
+        return annotationView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        //set intial region
+        let initialregion = MKCoordinateRegion(center: initialCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.25, longitudeDelta: 0.25))
+        mapView.setRegion(initialregion, animated: true)
+        // add the annotations
+        mapView.addAnnotations(hospitalAnnotations.hospitals)
+        checkLocationServices()
     }
-    */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+//        self.selectedAnnotation = view.annotation as? MKPointAnnotation
+//        info()
+//    }
+//
+//    func info(sender: Any) {
+//        print(selectedAnnotation?.coordinate)
+//    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        var selectedAnnotation = view.annotation
+        var link = hospitalData[(Int(((selectedAnnotation?.subtitle)!)!)!)-1].link
+        print(link)
+
+    }
+    
+       func setupLocationManager() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+        
+        func centerViewOnUserLOcation() {
+            if let location = locationManager.location?.coordinate {
+                let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+                mapView.setRegion(region, animated: true)
+            }
+        }
+        
+        func checkLocationServices() {
+            if CLLocationManager.locationServicesEnabled() {
+                setupLocationManager()
+                CheckLocationAuthorisation()
+            }else {
+                
+            }
+        }
+        
+        func CheckLocationAuthorisation() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                mapView.showsUserLocation = true
+                centerViewOnUserLOcation()
+                break
+            case .denied:
+                let alert = UIAlertController(title: "Location Service disabled for Symptracker", message: "We require Location Service to serve you better. You can turn it on in the settings", preferredStyle: .alert)
+                // add the button yes
+                alert.addAction(UIAlertAction(title: NSLocalizedString("Continue", comment: "Default action"), style: .default, handler: { _ in
+                    self.present(alert, animated: true, completion: nil)
+                }))
+
+                break
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                let alert = UIAlertController(title: "Location Service disabled for Symptracker", message: "We require Location Service to serve you better. You can turn it on in the settings", preferredStyle: .alert)
+                // add the button yes
+                alert.addAction(UIAlertAction(title: NSLocalizedString("Continue", comment: "Default action"), style: .default, handler: { _ in
+                    self.present(alert, animated: true, completion: nil)
+                }))
+                break
+            case .authorizedAlways:
+                break
+            }
+        }
+        
+    
 
 }
+
+extension HospitalsViewController: CLLocationManagerDelegate {
+       
+       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+       }
+       
+       func locationManager(_ manager: CLLocationManager, didChangeAuthorisation status: CLAuthorizationStatus) {
+           CheckLocationAuthorisation()
+       }
+   }
+
